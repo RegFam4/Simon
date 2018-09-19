e0da391cd2443d1baa2de0375369054509dba518
REGALADO-MORALES,ARM ADRIAN <rl106816@rlas-macair-22.rlas-116.org>
2018-09-19 12:18:12 -0500

    Initial Commit

---
+++ Simon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simon
+//
+//  Created by REGALADO-MORALES,ARM ADRIAN on 9/19/18.
+//  Copyright © 2018 REGALADO-MORALES,ARM ADRIAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
